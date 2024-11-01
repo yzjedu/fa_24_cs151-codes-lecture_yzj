4b72dcc0785d5e8d21a5e3ad69e19a0076e4462a
Zelalem Jembre Yalew <zizutg@gmail.com>
2024-11-01 08:45:57 -0400

    renaming and 09

---
+++ 08_for_while.py
@@ -0,0 +1,23 @@
+def count_grades_while(grade_list, average):
+    count = 0
+    index = 0
+    while index < len(grade_list):
+        if grade_list[index] >= average:
+            count += 1
+        index += 1
+    return count
+
+def count_grades_for(grade_list, average):
+    count = 0
+    for grade in grade_list:
+        if grade >= average:
+            count += 1
+    return count
+
+def main():
+    grades = [65, 70, 85, 90, 55, 76]
+    average = sum(grades) / len(grades)
+    result = count_grades_for(grades, average)
+    print(result)
+
+main()
