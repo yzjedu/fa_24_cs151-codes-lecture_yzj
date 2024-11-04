81f47ab6251dbe298c26b88941e3bb8e779263f6
Zelalem Jembre Yalew <zizutg@gmail.com>
2024-11-04 09:38:03 -0500

    grades_one_line.txt is added, 11_read_grades_proper.py updated

--- 11_read_grades_proper.py
+++ 11_read_grades_proper.py
@@ -14,10 +14,28 @@ def read_file_to_list_int(filename):
     # Return the list of grades
     return grades
 
+def read_file_to_list_split(filename):
+    # Open the file for reading
+    file_data = open(filename, 'r')
+    grades = []
+
+    # Read each line, convert to integer, and append to the grades list
+    for line in file_data:
+        line = line.split()
+        line = map(int, line)
+        grades.extend(line)
+
+    # Close the file
+    file_data.close()
+
+    # Return the list of grades
+    return grades
+
 def main():
     # Read the contents of 'grades.txt' and store them in the grades list
     grades = read_file_to_list_int("grades.txt")
-
+    #grades = read_file_to_list_split("grades_one_line.txt")
+    print(grades)
     # Calculate the average, minimum, and maximum of the grades
     average = sum(grades) / len(grades)
     minimum = min(grades)
